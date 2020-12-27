2785410c75cf271ae911250e6a503f6a3cbe98a0
jeff14994 <jeff14994@gmail.com>
2020-12-27 20:16:06 +0800

    Complete Day25 HW
    1. Utilized JupyterLab( a kind of VS Code )
    2. Used scrapy crawl [name(without .py)] to start crawling
    3. Great Resources: https://tinyurl.com/y7derx5t -> Scrapy Tutorial

---
+++ Day25/scrapy_demo/spiders/ettoday.py
@@ -0,0 +1,10 @@
+#import scrapy
+import scrapy
+
+class EttodaySpider(scrapy.Spider):
+    name = 'ettoday'
+    allowed_domains = ['www.ettoday.net']
+    start_urls = ['http://www.ettoday.net/']
+
+    def parse(self, response):
+        pass
